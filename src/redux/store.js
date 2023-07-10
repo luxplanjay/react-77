ed34bf73b4a198bb6f74343567e1666b8d6e901a
Alexander Repeta <alexander.repeta@gmail.com>
2023-07-10 20:25:42 +0300

    add examples

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,47 @@ import { configureStore } from '@reduxjs/toolkit';
 import { accountReducer } from './accountSlice';
 import { userReducer } from './userSlice';
 
+const customMiddleware = store => next => action => {
+  console.log('custom middleware!!!!!');
+  next(action);
+};
+
+const a1 = {
+  type: 'a1',
+  payload: 1,
+  meta: {
+    ga: true,
+  },
+};
+
+const a2 = {
+  type: 'a2',
+  payload: 2,
+  meta: {
+    ga: true,
+  },
+};
+
+const a3 = {
+  type: 'a3',
+  payload: 3,
+};
+
+const googleMiddleware = store => next => action => {
+  if (action?.meta?.ga) {
+    // GA.send(action);
+  }
+
+  console.log('googleMiddleware');
+  next(action);
+};
+
 export const store = configureStore({
   reducer: {
     user: userReducer,
     account: accountReducer,
+  },
+  middleware: getDefaultMiddleware => {
+    return [customMiddleware, ...getDefaultMiddleware(), googleMiddleware];
   },
 });
